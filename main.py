0bd821eb38a17e8cdde267c65f9117b9a71675a7
Tom Osika <26352734+tao558@users.noreply.github.com>
2021-12-08 15:47:27 -0500

    Refactor out to common api

--- main.py
+++ main.py
@@ -13,37 +13,17 @@ import util
 import monai
 import torch
 import itk
-# import astra
 import numpy as np
 import matplotlib.pyplot as plt
-import segmentation_models_pytorch as smp
 import shutil
 
 
 from mpl_toolkits.axes_grid1 import ImageGrid
-from ignite.engine import Events
 from monai.data import partition_dataset
 from pathlib import Path
-from monai.engines import SupervisedTrainer, SupervisedEvaluator
-from monai.handlers import ValidationHandler, CheckpointSaver, MeanAbsoluteError, MeanSquaredError
-from torch import optim, nn
+
 from monai.data import ArrayDataset, DataLoader
-from monai.transforms import (
-    Activations,
-    AddChannel,
-    AsDiscrete,
-    Compose,
-    EnsureType,
-    Resize,
-    LoadImage,
-    RandFlip,
-    RandZoom,
-    ScaleIntensity,
-    SqueezeDim,
-    Transform,
-    KeepLargestConnectedComponent,
-    SqueezeDim,
-)
+from dl_with_unet import *
 
 
 # Any and all data goes here
@@ -99,18 +79,6 @@ ExtractedDataDim = 2
 SourceImageType = itk.Image[PixelType, SourceDataDim]
 ImageType = itk.Image[PixelType, ExtractedDataDim]
 
-NETWORK_INPUT_SIZE = (256, 256)
-# Some other constants
-
-# Default number of epochs
-NUM_EPOCHS = 200
-
-BATCH_SIZE = 16
-
-BEST_MODEL_NAME = "best_model.pt"
-MODEL_SAVE_DIR = Path("./models")
-BEST_MODEL_PATH = MODEL_SAVE_DIR / BEST_MODEL_NAME
-
 # ITK stuff
 PixelType = itk.F
 InputDataDim = 3
@@ -119,69 +87,6 @@ ExtractedDataDim = 2
 RawDataImageType = itk.Image[PixelType, InputDataDim]
 ImageType = itk.Image[PixelType, ExtractedDataDim]
 
-NETWORK_INPUT_SIZE = (256, 256)
-
-
-# Define our transforms
-train_x_transforms = Compose(
-    [
-        LoadImage(image_only=True),
-        EnsureType(),
-        AddChannel(),
-        Resize(NETWORK_INPUT_SIZE),
-        # Resize(NETWORK_INPUT_SHAPE, mode="nearest"),
-        # # RandZoom(prob=0.5, min_zoom=0.9, max_zoom=1.3, mode="nearest"), # TODO: Make sure we dont center zoom at cut out the top. Uncomment
-        ScaleIntensity(),
-        RandFlip(prob=0.5, spatial_axis=1),
-    ]
-)
-train_y_transforms = Compose(
-    [
-        LoadImage(image_only=True),
-        EnsureType(),
-        AddChannel(),
-        Resize(NETWORK_INPUT_SIZE),
-        # Resize(NETWORK_INPUT_SHAPE, mode="nearest"),
-        # # RandZoom(prob=0.5, min_zoom=0.9, max_zoom=1.3, mode="nearest"), # TODO: Make sure we dont center zoom at cut out the top. Uncomment
-        ScaleIntensity(),
-        RandFlip(prob=0.5, spatial_axis=1),
-    ]
-)
-val_x_transforms = Compose(
-    [
-        LoadImage(image_only=True),
-        EnsureType(),
-        AddChannel(),
-        Resize(NETWORK_INPUT_SIZE),
-        # Resize(NETWORK_INPUT_SHAPE, mode="nearest"),
-        ScaleIntensity(),
-        # EnsureType(),
-    ]
-)
-
-val_y_transforms = Compose(
-    [
-        LoadImage(image_only=True),
-        EnsureType(),
-        AddChannel(),
-        Resize(NETWORK_INPUT_SIZE),
-        ScaleIntensity(),
-        # Resize(NETWORK_INPUT_SHAPE, mode="nearest"),
-        # EnsureType(),
-        # AsDiscrete(to_onehot=True, n_classes=3),
-    ]
-)
-
-itk_image_to_model_input = Compose(
-    [
-        EnsureType(data_type="tensor"),
-        AddChannel(),
-        Resize(NETWORK_INPUT_SIZE, mode="nearest"),
-        ScaleIntensity(),
-        AddChannel(),
-        EnsureType(),
-    ]
-)
 
 
 def _construct_parser():
@@ -347,14 +252,6 @@ def generate_synthetic_data(
         itk.imwrite(incomplete_sino, str(incomplete_sino_dir / (stem + ".mha")))
 
 
-# TODO: Weird that we have the "classes" keyword below.
-# This is only meant to be a proof of concept.
-def get_model():
-    return smp.Unet(
-        encoder_weights="imagenet", in_channels=1, classes=1, activation=None
-    )
-
-
 def get_data_loader(
     xdir, ydir, xtransforms=None, ytransforms=None, batch_size=BATCH_SIZE
 ):
@@ -385,7 +282,7 @@ def _display_images_in_grid(images, grid_shape, titles=None):
 
 # TODO:
 # Trying to figure out why sino_pred is darker than the rest
-# In doing so, I found out that ensuretype doesnt actually cast to a tensor, as I thought it did
+# In doing so, I found out that ensuretype doesnt actually cast to a tensor when itk image is passed
 def display_image_truth_and_prediction(sino_input, sino_gt, sino_pred, back_projector=None):
     images_to_display = [sino_input, sino_gt, sino_pred]
     titles = ["Input Sinogram", "GT Sinogram", "Predicted Sinogram"]
@@ -404,29 +301,6 @@ def display_image_truth_and_prediction(sino_input, sino_gt, sino_pred, back_proj
 
     _display_images_in_grid(images_to_display, grid_shape, titles)
 
-
-def run_inference(model, input_image, device):
-    model.eval()
-    model.to(device)
-
-    # Generate predictions
-    with torch.no_grad():
-        prediction = (
-            model(itk_image_to_model_input(input_image).to(device)).cpu().squeeze(0)
-        )
-
-    w, h = itk.size(input_image)
-    transform = Compose(
-        [
-            AddChannel(),
-            Resize((h, w), mode="nearest"),
-        ]
-    )
-
-    prediction = transform(prediction.squeeze(0)).squeeze(0)
-    prediction_image = util.image_from_array(prediction, reference_image=input_image)
-
-    return prediction_image
 
 def predict_image_and_show(
     model,
@@ -448,105 +322,6 @@ def predict_image_and_show(
     display_image_truth_and_prediction(image, gt, prediction, back_projector)
 
 
-
-def train_model(
-    model,
-    train_loader,
-    val_loader,
-    device,
-    num_epochs=NUM_EPOCHS,
-    loss_function=nn.L1Loss(),
-):
-
-    model.to(device)
-    # TODO: Keep track of the cumulative loss.
-    optimizer = optim.Adam(model.parameters())
-    metric_values = []
-    iter_losses = []
-    batch_sizes = []
-    epoch_loss_values = []
-
-    steps_per_epoch = len(train_loader.dataset) // train_loader.batch_size
-    if len(train_loader.dataset) % train_loader.batch_size != 0:
-        steps_per_epoch += 1
-
-    def trans_batch_val(x):
-        preds = []
-        labels = []
-        for d in x:
-            preds.append((d["pred"]))
-            labels.append(d["label"])
-        return preds, labels
-
-
-    def prepare_batch(batchdata, device, non_blocking):
-        imgs, masks = batchdata
-        return imgs.to(device), masks.to(device)
-
-    key_val_metric = {"MAE": MeanAbsoluteError(output_transform=trans_batch_val)}
-    MODEL_SAVE_DIR.mkdir(parents=True, exist_ok=True)
-    checkpoint_saver = CheckpointSaver(
-        save_dir=MODEL_SAVE_DIR,
-        save_dict={"model": model},
-        save_key_metric=True,
-        key_metric_negative_sign=True,
-        key_metric_filename=BEST_MODEL_NAME,
-    )
-
-    evaluator = SupervisedEvaluator(
-        device=device,
-        val_data_loader=val_loader,
-        network=model,
-        key_val_metric=key_val_metric,
-        prepare_batch=prepare_batch,
-        val_handlers=[checkpoint_saver],
-    )
-
-    trainer = SupervisedTrainer(
-        device=device,
-        max_epochs=num_epochs,
-        train_data_loader=train_loader,
-        network=model,
-        optimizer=optimizer,
-        loss_function=loss_function,
-        train_handlers=[ValidationHandler(1, evaluator)],
-        prepare_batch=prepare_batch,
-    )
-
-    @trainer.on(Events.ITERATION_COMPLETED)
-    def _end_iter(engine):
-        loss = np.average([o["loss"] for o in engine.state.output])
-        iter_losses.append(loss)
-        epoch = engine.state.epoch
-        epoch_len = engine.state.max_epochs
-        step = (engine.state.iteration % steps_per_epoch) + 1
-        batch_len = len(engine.state.batch[0])
-        batch_sizes.append(batch_len)
-
-        if step % 5 == 0:
-            print(
-                f"epoch {epoch}/{epoch_len}, step {step}/{steps_per_epoch}, training_loss = {loss:.4f}"
-            )
-
-    @trainer.on(Events.EPOCH_COMPLETED)
-    def run_validation(engine):
-        overall_average_loss = np.average(iter_losses, weights=batch_sizes)
-        epoch_loss_values.append(overall_average_loss)
-
-        # clear the contents of iter_losses and batch_sizes for the next epoch
-        iter_losses.clear()
-        batch_sizes.clear()
-
-        # fetch and report the validation metrics
-        mse = evaluator.state.metrics["MAE"]
-        metric_values.append(mse)
-        print(f"evaluation for epoch {engine.state.epoch},  MAE = {mse:.4f}")
-
-    trainer.run()
-
-    return metric_values
-
-
 def get_random_image_and_gt(image_dir, gt_dir):
     if isinstance(image_dir, str):
         image_dir = Path(image_dir)
@@ -563,12 +338,6 @@ def get_random_image_and_gt(image_dir, gt_dir):
     gt_path = str(gt_dir / image_fname)
 
     return itk.imread(str(random_im_path)), itk.imread(gt_path)
-
-
-def load_model(path=BEST_MODEL_PATH):
-    model = get_model()
-    model.load_state_dict(torch.load(str(path)))
-    return model
 
 
 def main():
